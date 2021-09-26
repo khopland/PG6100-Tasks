2b104ebd23be18a6679c8c131620b941578cfcc0
Kristoffer Larsen Hopland <24739853+97krihop@users.noreply.github.com>
2021-09-26 16:50:09 +0200

    task 9 done

--- auth/src/main/kotlin/org/tsdes/auth/Application.kt
+++ auth/src/main/kotlin/org/tsdes/auth/Application.kt
@@ -1,17 +1,18 @@
 package org.tsdes.auth
+
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @SpringBootApplication(scanBasePackages = ["org.tsdes"])
-class Application{
-
+class Application {
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
+
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
 }
